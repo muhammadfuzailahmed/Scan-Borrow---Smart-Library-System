b141e08ef5d195c4494e88bf7c3ee0cbf327ca7b
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2026-05-30 19:55:27 +0500

    feat(app): add routes to scan QR code and search book pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,26 @@ import { BookOpen, LogIn, Eye, EyeClosed } from "lucide-react";
 import Button from "./Components/Button/Button";
 import { Route, Routes } from "react-router-dom";
 import Login from "./Components/Login/Login";
+import StudentLayout from "./Components/Student/Layout/Layout"
+import StudentDashboard from "./Components/Student/Dashboard/Dashboard"
+import SearchBook from "./Components/Student/SearchBook/SearchBook";
+import ScanQR from "./Components/Student/ScanQR/ScanQR";
 
 function App() {
   return (
+    <>
+    
     <Routes>
       <Route path="/" element={<Login />} />
+
+    <Route path="/student" element={<StudentLayout />}>
+      <Route path="dashboard" element={<StudentDashboard />}/>
+      <Route path="search-books" element={<SearchBook />}/>
+      <Route path="scan-qr" element={<ScanQR />} />
+    </Route>
+
     </Routes>
+    </>
 
   )
 }
