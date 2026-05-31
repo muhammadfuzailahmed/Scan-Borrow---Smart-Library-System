6a6088f51b3cfbc76a7b7e3f81fbe86f247aaa2e
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2026-05-31 19:19:33 +0500

    feat(scan QR code): Add success modal

--- src/Components/Student/ScanQR/ScanQR.jsx
+++ src/Components/Student/ScanQR/ScanQR.jsx
@@ -2,11 +2,17 @@ import { useEffect, useRef, useState } from "react";
 import { Html5Qrcode } from "html5-qrcode";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { X, Check, Search, House } from "lucide-react";
+import Button from "../../Button/Button"
+import { useNavigate } from "react-router-dom";
 
 function ScanQR() {
   const scannerRef = useRef(null);
+  const navigate = useNavigate();
   const [scannedValue, setScannedValue] = useState("");
   const [userId, setUserId] = useState(JSON.parse(localStorage.getItem("currentUserId")))
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
+  const [bookData, setBookData] = useState({});
 
   useEffect(() => {
     const scanner = new Html5Qrcode("qr-reader");
@@ -20,6 +26,8 @@ function ScanQR() {
                     QRcode
                 }
             )
+            setBookData(response?.data?.borrowedBook)
+            setShowSuccessModal(true)
             toast.success("Book Borrowed Succesfully!")
         } catch (error) {
             toast.error(error.response?.data?.message)
@@ -68,8 +76,74 @@ function ScanQR() {
     };
   }, []);
 
+  const handleCloseShowSuccessModalButton = () => {
+    setShowSuccessModal(false)
+  }
+
+  const handleSearchButon = () => {
+    navigate("/student/search-books")
+  }
+
+  const handleDashboardButon = () => {
+    navigate("/student/dashboard")
+  }
+
   return (
     <main className="mx-auto max-w-4xl px-6 py-8">
+      <div>
+        {
+          showSuccessModal
+          &&
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[95vw] md:w-[30vw] py-8 bg-gray-100 rounded-md z-10">
+            <div onClick={handleCloseShowSuccessModalButton} className="absolute right-2 top-2">
+              <X size={22} />
+            </div>
+            <div className="mt-12">
+              <div className="flex items-center justify-center">
+                <Check size={38} color="green" strokeWidth={5}/>
+              </div>
+              <div>
+                <p className="text-green-500 text-lg font-bold text-center mt-2">Book Borrowed Successfully!</p>
+                <p className="mt-1 text-center">The book has been issued to you</p>
+                <div className="flex items-center justify-between px-1 py-2 border-b-2 border-b-gray-300 w-[90%] mx-auto">
+                  <p className="font-semibold">Transaction Code</p>
+                  <p>{bookData?.transactionCode}</p>
+                </div>
+
+              <div className="flex items-center justify-between px-1 py-2 border-b-2 border-b-gray-300 w-[90%] mx-auto">
+                  <p className="font-semibold">Book Name</p>
+                  <p>{bookData?.transactionCode}</p>
+                </div>
+
+                <div className="flex items-center justify-between px-1 py-2 border-b-2 border-b-gray-300 w-[90%] mx-auto">
+                  <p className="font-semibold">Copy Code</p>
+                  <p>{bookData?.transactionCode}</p>
+                </div>
+
+                <div className="flex items-center justify-between px-1 py-2 border-b-2 border-b-gray-300 w-[90%] mx-auto">
+                  <p className="font-semibold">Issue Date</p>
+                  <p>{bookData?.issueDate}</p>
+                </div>
+
+                <div className="flex items-center justify-between px-1 py-2 border-b-2 border-b-gray-300 w-[90%] mx-auto">
+                  <p className="font-semibold">Due Date</p>
+                  <p>{bookData?.dueDate}</p>
+                </div>
+
+                <div className="bg-green-100 w-[90%] mx-auto mt-4 p-1 rounded-sm">
+                  <p className="text-green-500">Tip: Please return  the book on or before due date to avoid any late fines</p>
+                </div>
+
+                <div className="flex items-center gap-4 w-[90%] mx-auto mt-6">
+                  <Button onClick={handleSearchButon} icon={<Search size={26}/>} title="Search Books"/>
+                  <Button onClick={handleDashboardButon} icon={<House size={26}/>} title="Dashboard"/>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        }
+      </div>
       <div className="rounded-[32px] bg-white p-6 shadow-sm">
         <h1 className="text-3xl font-bold text-slate-950">Scan Book QR</h1>
         <p className="mt-2 text-slate-500">
