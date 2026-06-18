402c91d9fa4a724cb3965a236fbc570f4c7e0458
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2026-06-18 22:31:19 +0500

    feat(student - book copies): add add book copy button and add book copy form

--- src/Components/Admin/BookCopies/BookCopies.jsx
+++ src/Components/Admin/BookCopies/BookCopies.jsx
@@ -1,17 +1,24 @@
 import { BookCopy, CheckCircle, XCircle } from "lucide-react";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 function BookCopies() {
   const [bookCopies, setBookCopies] = useState([]);
+  const [showAddBookModal, setShowAddBookModal] = useState(false);
+  const [copyCode, setCopyCode] = useState("");
+  const [QRcode, setQRcode] = useState("");
+  const [bookName, setBookName] = useState("");
+  const [bookId, setBookId] = useState("");
+  const [books, setBooks] = useState([]);
 
   const fetchBookCopies = async () => {
     try {
       const response = await axios.get(
         `${import.meta.env.VITE_ADMIN_BACKEND_URL}/admin-book-copies-details`,
         {
-          withCredentials: true
-        }
+          withCredentials: true,
+        },
       );
 
       setBookCopies(response?.data?.bookCopies || []);
@@ -20,22 +27,142 @@ function BookCopies() {
     }
   };
 
+  const fetchBooks = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_ADMIN_BACKEND_URL}/admin-book-details`,
+        {
+          withCredentials: true,
+        },
+      );
+      setBooks(response?.data?.books || []);
+    } catch (error) {
+      console.log("Error fetching admin books", error);
+    }
+  };
+
+  const handleAddBookCopyButton = async () => {
+    if(!bookId || !copyCode ||!QRcode) {
+      toast.error("Fill all required fields!")
+      return;
+    }
+    console.log(bookId)
+    try {
+      await axios.post(`${import.meta.env.VITE_ADMIN_BACKEND_URL}/add-book-copy`, {
+        bookId,
+        copyCode,
+        QRcode
+      },
+    {
+      withCredentials: true
+    }
+  )
+  toast.success("Book copy added")
+  setShowAddBookModal(false)
+  setBookId("")
+  setBookName("")
+  setCopyCode("")
+  setQRcode("")
+    } catch (error) {
+      toast.error(error.response?.data?.message)
+    }
+
+  }
+
   useEffect(() => {
     fetchBookCopies();
+    fetchBooks();
   }, []);
 
   return (
-    <main className="mx-auto max-w-7xl px-6 py-8">
-      <section className="mb-6 rounded-[32px] bg-gradient-to-br from-slate-900 to-indigo-900 p-8 text-white shadow-lg">
+    <main className="mx-auto max-w-7xl px-6 py-8 relative">
+      {showAddBookModal && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4">
+          <div className="w-full max-w-lg rounded-2xl bg-white p-6 shadow-xl">
+            <div className="mb-5 flex items-center justify-between">
+              <h2 className="text-xl font-bold">Add New Book Copy</h2>
+
+              <button
+                onClick={() => setShowAddBookModal(false)}
+                className="text-2xl font-bold text-gray-500 hover:text-black hover:cursor-pointer"
+              >
+                ×
+              </button>
+            </div>
+
+            <div>
+              <label className="mb-1 block font-medium">Select Book</label>
+
+              <select
+                value={bookId}
+                onChange={(e) => setBookId(e.target.value)}
+                className="w-full rounded-lg border p-3"
+              >
+                <option value="">Select a book</option>
+
+                {books.map((book) => (
+                  <option key={book.bookId} value={book.bookId}>
+                    {book.bookName}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div>
+              <label className="mb-1 block font-medium">Copy Code</label>
+              <input
+                type="text"
+                value={copyCode}
+                onChange={(e) => setCopyCode(e.target.value)}
+                className="w-full rounded-lg border p-3"
+              />
+            </div>
+
+            <div>
+              <label className="mb-1 block font-medium">QRCode Value</label>
+              <input
+                type="text"
+                value={QRcode}
+                onChange={(e) => setQRcode(e.target.value)}
+                className="w-full rounded-lg border p-3"
+              />
+            </div>
+
+            <div className="flex justify-end gap-3 pt-2">
+              <button
+                type="button"
+                onClick={() => setShowAddBookModal(false)}
+                className="rounded-xl border px-4 py-2 font-medium hover:cursor-pointer"
+              >
+                Cancel
+              </button>
+
+              <button
+                onClick={handleAddBookCopyButton}
+                className="rounded-xl bg-indigo-600 px-4 py-2 font-semibold text-white hover:bg-indigo-700  hover:cursor-pointer"
+              >
+                Add Book Copy
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="absolute right-10">
+        <button
+          onClick={() => setShowAddBookModal(true)}
+          className="rounded-xl bg-indigo-600 px-4 py-2 font-semibold text-white transition hover:bg-indigo-700 hover:cursor-pointer"
+        >
+          + Add Book Copy
+        </button>
+      </div>
+      <section className="my-12 rounded-[32px] bg-gradient-to-br from-slate-900 to-indigo-900 p-8 text-white shadow-lg">
         <div className="flex items-center gap-3">
           <div className="rounded-2xl bg-white/10 p-3">
             <BookCopy size={28} />
           </div>
 
           <div>
-            <p className="text-sm font-semibold text-indigo-200">
-              Admin Panel
-            </p>
+            <p className="text-sm font-semibold text-indigo-200">Admin Panel</p>
             <h1 className="text-4xl font-bold">Book Copies</h1>
           </div>
         </div>
